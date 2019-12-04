1090f6abefede3609dc4c90ab2209c83aeeed7bc
Samuel Cristiano Silva Xavier <samuelxavier@aplic.com.br>
2019-12-04 14:42:09 -0300

    Primeiro comi

---
+++ Teste.php
@@ -0,0 +1,17 @@
+<?php
+
+class Teste
+{
+    public static function metodo()
+    {
+        echo "Teste";
+    }
+}
+
+class Teste2
+{
+    public static function metodo()
+    {
+        echo "Teste 2";
+    }
+}
